028f45a2e7f9e90f89121c608e82c16a42019e73
John <mijailhernandez@gmail.com>
2021-01-30 00:14:24 -0500

    Added price choice with input range.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-const msgDiscount = document.querySelector('.card__time-discount');
 const btnToggle = document.getElementById('btntoggle');
+const msgDiscount = document.querySelector('.card__time-discount');
 const screen = window.matchMedia("(max-width: 900px)");
-const views = document.getElementById('views');
+const inputRange = document.getElementById('input-range');
+const views = document.getElementById('pageviews');
+const monthlyPrice = document.getElementById('price')
 
 screen.addListener( () => {
     if (screen.matches) { // If media query matches
@@ -11,16 +13,45 @@ screen.addListener( () => {
     }
 })
 
-// document.addEventListener('DOMContentLoaded', ()=> {
-//     if (document.body.clientWidth >= 960) {
-//         msgDiscount.textContent = "25% discount";
-//     } else {
-//         msgDiscount.textContent = "-25%";
-//     }
-// })
-
+// test
 btnToggle.addEventListener('click', ()=> {
     if (btnToggle.checked) {
         console.log('mensual');
+    }
+})
+
+inputRange.addEventListener('input', ()=> {
+    let valueInputRange = parseInt(inputRange.value);
+    console.log('working');
+
+    switch (valueInputRange) {
+        case 1:
+            console.log('working');
+            views.textContent = "10K";
+            monthlyPrice.textContent = "$8.00";
+            break;
+
+        case 2:
+            views.textContent = "50K";
+            monthlyPrice.textContent = "$12.00";
+            break;
+
+        case 3:
+            views.textContent = "100K";
+            monthlyPrice.textContent = "$16.00";
+            break;
+
+        case 4:
+            views.textContent = "500K";
+            monthlyPrice.textContent = "$24.00";
+            break;
+
+        case 5:
+            views.textContent = "1M";
+            monthlyPrice.textContent = "$36.00";
+            break;
+    
+        default:
+            break;
     }
 })
