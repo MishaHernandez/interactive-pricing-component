dabc56afd1e1589e1935006935f2e7b88b564237
John <mijailhernandez@gmail.com>
2021-01-30 03:29:27 -0500

    Added discount calculation.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const btnToggle = document.getElementById('btntoggle');
 const msgDiscount = document.querySelector('.card__time-discount');
 const screen = window.matchMedia("(max-width: 900px)");
 const inputRange = document.getElementById('input-range');
-const views = document.getElementById('pageviews');
+const pageviews = document.getElementById('pageviews');
 const monthlyPrice = document.getElementById('price')
 
 screen.addListener( () => {
@@ -13,45 +13,56 @@ screen.addListener( () => {
     }
 })
 
-// test
-btnToggle.addEventListener('click', ()=> {
-    if (btnToggle.checked) {
-        console.log('mensual');
-    }
-})
+function monthlyBilling(price) {
+    let newPrice = price - (price * 0.25);
+    monthlyPrice.textContent = `${newPrice}`;
+}
 
 inputRange.addEventListener('input', ()=> {
     let valueInputRange = parseInt(inputRange.value);
-    console.log('working');
 
     switch (valueInputRange) {
         case 1:
-            console.log('working');
-            views.textContent = "10K";
-            monthlyPrice.textContent = "$8.00";
+            if (btnToggle.checked) { monthlyBilling(8); break };
+            pageviews.textContent = "10K";
+            monthlyPrice.textContent = "8";
             break;
 
         case 2:
-            views.textContent = "50K";
-            monthlyPrice.textContent = "$12.00";
+            if (btnToggle.checked) { monthlyBilling(12); break};
+            pageviews.textContent = "50K";
+            monthlyPrice.textContent = "12";
             break;
 
         case 3:
-            views.textContent = "100K";
-            monthlyPrice.textContent = "$16.00";
+            if (btnToggle.checked) { monthlyBilling(16); break};
+            pageviews.textContent = "100K";
+            monthlyPrice.textContent = "16";
             break;
 
         case 4:
-            views.textContent = "500K";
-            monthlyPrice.textContent = "$24.00";
+            if (btnToggle.checked) { monthlyBilling(24); break};
+            pageviews.textContent = "500K";
+            monthlyPrice.textContent = "24";
             break;
 
         case 5:
-            views.textContent = "1M";
-            monthlyPrice.textContent = "$36.00";
+            if (btnToggle.checked) { monthlyBilling(36); break};
+            pageviews.textContent = "1M";
+            monthlyPrice.textContent = "36";
             break;
     
         default:
             break;
+    }
+})
+
+btnToggle.addEventListener('click', () => {
+    let currentPrice = monthlyPrice.textContent;
+
+    if (btnToggle.checked) {
+        monthlyPrice.textContent = `${currentPrice - (currentPrice * 0.25)}`;
+    } else {
+        monthlyPrice.textContent = (4 * currentPrice) / 3;
     }
 })
